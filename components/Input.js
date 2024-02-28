d5e6fab87f5bacdb238f64d687064912a70284ca
Kantapon Sritiyot <kantapon.sritiyot@gmail.com>
2024-02-28 07:42:20 +0000

    Add Input component

---
+++ components/Input.js
@@ -0,0 +1,100 @@
+import React, { useEffect, useRef, useState } from "react";
+import { NumericFormat } from "react-number-format";
+
+const normalizeNumericValue = (value) => {
+  const number = Number(value.replaceAll(",", ""));
+  if (isNaN(number)) return 0;
+  return number;
+};
+
+const numericToLocaleString = ({
+  value,
+  decimal,
+}) => {
+  if (!value) return "";
+  return value.toLocaleString(undefined, {
+    minimumFractionDigits: decimal,
+  });
+};
+
+const Input = ({
+  align = "left",
+  disabled = false,
+  isError = false,
+  placeholder = "",
+  supportText,
+  onChange,
+  value,
+  title,
+  unit,
+  className,
+  isSupportIcon,
+  inputIcon,
+  decimal,
+  maxRange,
+  id,
+  isInitialValueToZero = true,
+  isBlurInput,
+}) => {
+  const [isFocus, setIsFocus] = useState(false);
+  const [valueInput, setValueInput] = useState<string>(
+    numericToLocaleString({ value, decimal })
+  );
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    if (!isFocus) {
+      setValueInput(numericToLocaleString({ value, decimal }));
+    }
+  }, [decimal, isFocus, value]);
+
+  useEffect(() => {
+    if (isBlurInput) {
+      inputRef.current?.blur();
+      setIsFocus(false);
+    }
+  }, [isBlurInput]);
+
+  return (
+    <div className={className}>
+      <span>{title}</span>
+      <div>
+        <NumericFormat
+          decimalScale={decimal}
+          disabled={disabled}
+          getInputRef={inputRef}
+          id={id}
+          inputMode="decimal"
+          isAllowed={(values) => {
+            const { floatValue } = values;
+            if (maxRange) return !floatValue || floatValue <= maxRange;
+            return !floatValue;
+          }}
+          onBlur={(e) => {
+            setIsFocus(false);
+            if (!isInitialValueToZero && e.target.value === "") {
+              return onChange?.(undefined);
+            }
+            const number = normalizeNumericValue(e.target.value);
+            onChange?.(number);
+          }}
+          onFocus={() => {
+            setIsFocus(true);
+          }}
+          onValueChange={(values) => {
+            if (isFocus) onChange?.(values.floatValue);
+          }}
+          placeholder={placeholder}
+          thousandSeparator
+          value={valueInput}
+        />
+        <div className="absolute right-0 top-1/2 -translate-x-0 -translate-y-1/2 transform pl-sm">
+          {inputIcon && inputIcon}
+          {unit && <div>{unit}</div>}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Input;
--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,25 @@
+import { useState } from 'react'
 import Head from 'next/head'
-import Header from '@components/Header'
 import Footer from '@components/Footer'
+import Input from '@components/Input'
 
 export default function Home() {
+  const [value, setValue] = useState(0);
   return (
     <div className="container">
       <Head>
         <title>Next.js Starter!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
       <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+        <Input
+          placeholder={'0.'.padEnd(decimal + 2, '0')}
+          decimal={2}
+          maxRange={99999999.99}
+          onChange={(value) => setValue(value)}
+          value={value}
+        />
       </main>
-
       <Footer />
     </div>
   )
